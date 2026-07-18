a4a19ba584b3e64ba1e4f2d33885ced60157dfc2
zhou <zhou@zhoudeMacBook-Air.local>
2026-07-19 07:18:23 +0800

    chore: clean up autocore, daed, and redundant dependencies globally

--- Scripts/Packages-MT7986.sh
+++ Scripts/Packages-MT7986.sh
@@ -50,11 +50,7 @@ UPDATE_PACKAGE() {
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main" "" "mihomo-meta mihomo-alpha mihomo"
 rm -rf ./OpenWrt-nikki/mihomo-alpha
 
-# Upgrade golang to 1.26 to fix daed build error "requires go >= 1.26.0"
-rm -rf ../feeds/packages/lang/golang
-git clone --depth=1 --single-branch --branch 26.x https://github.com/sbwml/packages_lang_golang.git ../feeds/packages/lang/golang
 
-UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "kix"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
 UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
 UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
